32f229a4f9b2d9e9e3fd15f8542b0bf2ffc9ba6c
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-10-21 16:05:19 +0200

    starting procedure for trying to recreate Lyder's code

---
+++ R/comparison_CV.R
@@ -0,0 +1,6 @@
+# File to try to recreate Lyder's results and compare them
+# But first just recreate them
+
+# Plan:
+# Make a modified "MakeStacks" that tries to recreate Lyder's procedure
+# Then copy the cross-validation procedure and fit models with the new stacks in one big run
